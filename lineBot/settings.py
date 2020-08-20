a3099a8d0ac53f23d559167e7f0427a0e9cd20e2
eiki <53928021+N-Eiki@users.noreply.github.com>
2020-08-20 19:24:17 +0900

    Update settings.py

--- lineBot/settings.py
+++ lineBot/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g4bgm31&bh1%%-u2ji(xlq=0ug%p32dws%w)k$*mqh%rbh)7m1'
+# SECRET_KEY = 'g4bgm31&bh1%%-u2ji(xlq=0ug%p32dws%w)k$*mqh%rbh)7m1'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
